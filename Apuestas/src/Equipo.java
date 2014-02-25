877ea4491ddef3f5c1d1ca43345a82d8dbe64635
Ivan Toledo <itoledo2404@gmail.com>
2014-02-25 13:09:43 +0100

    Modify Form, Methods(Class Equipo)

--- Apuestas/src/Equipo.java
+++ Apuestas/src/Equipo.java
@@ -12,27 +12,52 @@ public class Equipo implements Serializable{
 	//creo el constructor equipo con los parametros necesarios
 	public Equipo(String nom, int golesF, int golesC, int partidosG, int partidosP ) {
 		// TODO Auto-generated constructor stub
-		//
+		//inicializo los atributos
+		nombreEquipo = nom;
+		golesFavor = golesF;
+		golesEnContra = golesC;
+		partidosGanados = partidosG;
+		partidosPerdidos = partidosP;
 	}
+	//metodo para pasar valor al attr nombreEquipo
 	public void setNombre(String nom ){
+		nombreEquipo = nom;
 	}
-	public void getNombre(String nom){	
+	//metodo para devolver el valor de nombreEquipo
+	public String getNombre(){	
+		return nombreEquipo;
 	}
+	//metodo para pasar valor al attr golesFavor
 	public void setGolesFavor(int golesF ){
+		golesFavor = golesF;
 	}
-	public void getGolesFavor(int golesF){	
+	//metodo para devolver el valor de golesFavor
+	public int getGolesFavor(){	
+		return golesFavor;
 	}
+	//metodo para pasar valor al attr golesEnContra
 	public void setGolesContra(int golesC ){
+		golesEnContra = golesC;
 	}
-	public void getGolesContra(int golesC){	
+	//metodo para devolver el valor de golesEnContra
+	public int getGolesContra(){	
+		return golesEnContra ;
 	}
+	//metodo para pasar valor al attr partidosGanados
 	public void setGolesPartidosGanados(int partidosG){
+		partidosGanados = partidosG;
 	}
-	public void getGolesPartidosGanado(int partidosG){	
+	//metodo para devolver el valor de partidosGanados
+	public int getGolesPartidosGanado(){	
+		return partidosGanados;
 	}
+	//metodo para pasar valor al attr partidosPerdido
 	public void setGolesPartidosPerdidos(int partidosP){
+		partidosPerdidos = partidosP;
 	}
-	public void getGolesPartidosPerdidos(int partidosP){	
+	//metodo para devolver el valor de partidosPerdidos
+	public int getGolesPartidosPerdidos(){
+		return partidosPerdidos;
 	}
 	
 }
--- Apuestas/src/VentanaEquipo.java
+++ Apuestas/src/VentanaEquipo.java
@@ -9,12 +9,19 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.*;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class VentanaEquipo extends JFrame {
 	
 	private JPanel contentPane;
 	//Creo el objeto equipo
 	private Equipo equipo;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 	/**
 	 * Create the frame.
 	 */
@@ -22,7 +29,7 @@ public class VentanaEquipo extends JFrame {
 		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 339);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -51,7 +58,52 @@ public class VentanaEquipo extends JFrame {
 				} // fin de catch
 			}
 		});
-		btnGuardarFichero.setBounds(295, 228, 129, 23);
+		btnGuardarFichero.setBounds(295, 267, 129, 23);
 		contentPane.add(btnGuardarFichero);
+		
+		JLabel lblNombreEquipo = new JLabel("Nombre Equipo");
+		lblNombreEquipo.setBounds(10, 11, 109, 14);
+		contentPane.add(lblNombreEquipo);
+		
+		textField = new JTextField();
+		textField.setBounds(118, 8, 232, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblGolesAFavor = new JLabel("Goles a Favor");
+		lblGolesAFavor.setBounds(10, 57, 109, 14);
+		contentPane.add(lblGolesAFavor);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(118, 54, 232, 20);
+		contentPane.add(textField_1);
+		
+		JLabel lblGolesEnContra = new JLabel("Goles en Contra");
+		lblGolesEnContra.setBounds(10, 104, 109, 14);
+		contentPane.add(lblGolesEnContra);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(118, 101, 232, 20);
+		contentPane.add(textField_2);
+		
+		JLabel lblPartidosGanados = new JLabel("Partidos Ganados");
+		lblPartidosGanados.setBounds(10, 155, 109, 14);
+		contentPane.add(lblPartidosGanados);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(118, 149, 232, 20);
+		contentPane.add(textField_3);
+		
+		JLabel lblPartidosPerdidos = new JLabel("Partidos Perdidos");
+		lblPartidosPerdidos.setBounds(10, 201, 109, 14);
+		contentPane.add(lblPartidosPerdidos);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(118, 198, 232, 20);
+		contentPane.add(textField_4);
 	}
 }
