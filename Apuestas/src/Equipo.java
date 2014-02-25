a300edc74b7fd7b24a2661f5c272c61ca9cc03fc
Ivan Toledo <itoledo2404@gmail.com>
2014-02-25 12:22:11 +0100

    Create New Attr, method

--- Apuestas/src/Equipo.java
+++ Apuestas/src/Equipo.java
@@ -1,8 +1,39 @@
+import java.io.Serializable;;
 
-public class Equipo {
-
-	public Equipo() {
+public class Equipo implements Serializable{
+	
+	//Declaro los atributos del equipo
+	
+	private String nombreEquipo;
+	private int golesFavor;
+	private int golesEnContra;
+	private int partidosGanados;
+	private int partidosPerdidos;
+	//creo el constructor equipo con los parametros necesarios
+	public Equipo(String nom, int golesF, int golesC, int partidosG, int partidosP ) {
 		// TODO Auto-generated constructor stub
+		//
 	}
-
+	public void setNombre(String nom ){
+	}
+	public void getNombre(String nom){	
+	}
+	public void setGolesFavor(int golesF ){
+	}
+	public void getGolesFavor(int golesF){	
+	}
+	public void setGolesContra(int golesC ){
+	}
+	public void getGolesContra(int golesC){	
+	}
+	public void setGolesPartidosGanados(int partidosG){
+	}
+	public void getGolesPartidosGanado(int partidosG){	
+	}
+	public void setGolesPartidosPerdidos(int partidosP){
+	}
+	public void getGolesPartidosPerdidos(int partidosP){	
+	}
+	
 }
+
--- Apuestas/src/VentanaEquipo.java
+++ Apuestas/src/VentanaEquipo.java
@@ -4,22 +4,60 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.*;
 
 public class VentanaEquipo extends JFrame {
-
+	//Declaro los atributos del equipo
+	private String nom = "default";
+	private int golesF = 0;
+	private int golesC = 0;
+	private int partidosG = 0;
+	private int partidosP = 0;
+	
 	private JPanel contentPane;
-
+	//Creo el objeto equipo
+	Equipo equipo;
 	/**
 	 * Create the frame.
 	 */
 	public VentanaEquipo() {
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnGuardarFichero = new JButton("Guardar Fichero");
+		btnGuardarFichero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//genero el objeto salida para la escritura del fichero
+				ObjectOutputStream salida; 
+				try
+				{
+					// abre el archivo
+					salida = new ObjectOutputStream (new FileOutputStream ("clientes.ser"));
+					// crea un registro nuevo
+					equipo = new Equipo(nom, golesF, golesC, partidosG, partidosP );
+					// envía el registro como salida
+					salida.writeObject( equipo );
+					// cierra el archivo
+					if ( salida != null )
+						salida.close();
+				}//fin de try
+				catch ( IOException ioException )
+				{
+				 System.err.println( "Error al abrir el archivo." );
+				} // fin de catch
+			}
+		});
+		btnGuardarFichero.setBounds(295, 228, 129, 23);
+		contentPane.add(btnGuardarFichero);
 	}
-
 }
